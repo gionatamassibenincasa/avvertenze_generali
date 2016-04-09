46921d29d63af7b7396f6124c235ac16e8d15f35
Gionata Massi (in qualità di docente d'Informatica) <gionata.massi@savoiabenincasa.it>
2016-04-09 11:07:30 +0200

    Some more concepts of behaviourism

--- Avvertenze_Generali.mm
+++ Avvertenze_Generali.mm
@@ -55,7 +55,7 @@
 <node TEXT="Teorie dell&apos;apprendimento e psicologia dell&apos;educazione" ID="ID_1422292693" CREATED="1460182527908" MODIFIED="1460182552199">
 <node TEXT="Comportamentismo" ID="ID_1015287476" CREATED="1460182964837" MODIFIED="1460182970282">
 <node TEXT="Introduzione" ID="ID_1283369102" CREATED="1460184467648" MODIFIED="1460184475461">
-<node ID="ID_816649685" CREATED="1460184520253" MODIFIED="1460184677422"><richcontent TYPE="NODE">
+<node ID="ID_816649685" CREATED="1460184520253" MODIFIED="1460187018725"><richcontent TYPE="NODE">
 
 <html>
   <head>
@@ -65,15 +65,47 @@
     <p>
       Il comportamentismo studia il meccanismo Stimolo S (esterno) -&gt; Risposta R (comportamento). Quando la risposta R ad un dato stimolo S diviene stabile si ha l'<b>apprendimento</b>.
     </p>
+    <p>
+      La mente &#232; una sorta di &quot;black box&quot; il cui funzionamento interno &#232; inconoscibile e, per certi aspetti, irrilevante.
+    </p>
+    <p>
+      L'oggetto dell'indagine dei comportamentisti &#232; la relazione tra gli stimoli ambientali e le risposte comportamentali.
+    </p>
+    <p>
+      Il meccanismo del condizionamento, cio&#232; l'associazione ripetuta di uno stimolo con una risposta ad esso non direttamente correlata, &#232; studiato e approfondito dai comportamentisti.
+    </p>
   </body>
 </html>
 
 </richcontent>
 </node>
 </node>
-<node TEXT="Pavlov" ID="ID_1187425509" CREATED="1460184476208" MODIFIED="1460184480108"/>
-<node TEXT="Thorndike" ID="ID_544706949" CREATED="1460184483383" MODIFIED="1460184512715"/>
-<node TEXT="Skinner" ID="ID_1493424747" CREATED="1460184513574" MODIFIED="1460184517306"/>
+<node TEXT="Pavlov" ID="ID_1187425509" CREATED="1460184476208" MODIFIED="1460184480108">
+<node TEXT="1849 -1936 - Russia - Medico" ID="ID_283143287" CREATED="1460187082084" MODIFIED="1460187713678"/>
+<node TEXT="1903 - Il riflesso condizionato. Dagli studi sull&apos;apparato diregente dei cani Pavlov not&#xf2; come il cervello controlli anche i riflessi. Associando il suono di una campanella (stimolo neutro) alla somministrazione di carne (stimolo incondizionato) si ottiene la salivazione (risposta incondizionata) anche con il solo suono della campanella (risposta condizionata o riflessa)." ID="ID_1174710584" CREATED="1460187308590" MODIFIED="1460187571723"/>
+</node>
+<node TEXT="Watson" ID="ID_1552000490" CREATED="1460186573498" MODIFIED="1460186577279">
+<node TEXT="1878 - 1958 - Stati Uniti - Psicologo &quot;padre&quot; del comportamentismo." ID="ID_1018348293" CREATED="1460186577295" MODIFIED="1460187737550"/>
+<node TEXT="1913 - &quot;Psycology as a Behaviourist Views It&quot;" ID="ID_252734568" CREATED="1460187623779" MODIFIED="1460187663709"/>
+<node TEXT="Principi della frequenza e della recentezza: tanto pi&#xf9; spesso o tanto pi&#xf9; recentemente un&apos;associazione si &#xe8; verificata, con tanta maggiore probabilit&#xe0; si verificher&#xe0;." ID="ID_1827863595" CREATED="1460188113831" MODIFIED="1460188264273"/>
+</node>
+<node TEXT="Thorndike" ID="ID_544706949" CREATED="1460184483383" MODIFIED="1460184512715">
+<node TEXT="1874 - 1949 - Stati Uniti - Psicologo" ID="ID_714903423" CREATED="1460188327356" MODIFIED="1460188344764"/>
+<node TEXT="Introduce l&apos;idea del rinforzo positivo e dell&apos;apprendimento per prove ed errori" ID="ID_71607920" CREATED="1460188349557" MODIFIED="1460188462709"/>
+<node TEXT="Leggi dell&apos;apprendimento" ID="ID_797212626" CREATED="1460188463357" MODIFIED="1460188470725">
+<node TEXT="Legge dell&apos;esercizio: l&apos;apprendimento &#xe8; graduale e migliora con la ripetizione delle prove" ID="ID_1434955272" CREATED="1460188471498" MODIFIED="1460188497270"/>
+<node TEXT="Legge dell&apos;effetto: l&apos;apprendimento avviene in funzione delle conseguenze del comportamento - le azioni che tendono ad essere ripetute sono quelle che portano ad una riduzione dello stato di bisogno o procurano una ricompensa (benessere)" ID="ID_1972746843" CREATED="1460188498590" MODIFIED="1460188587747"/>
+<node TEXT="Legge del trasferimento: le risposte ad una situzione saranno sviluppate anche in situazioni simili" ID="ID_1669713374" CREATED="1460188590270" MODIFIED="1460188656791"/>
+</node>
+</node>
+<node TEXT="Skinner" ID="ID_1493424747" CREATED="1460184513574" MODIFIED="1460184517306">
+<node TEXT="1904 - 1990 - Stati Uniti - Psicologo" ID="ID_97520591" CREATED="1460188714885" MODIFIED="1460188732466"/>
+<node TEXT="Promuove una teoria della scienza che chiama &quot;comportamentismo radicale&quot; con i concetti di &quot;rinforzo&quot; e &quot;condizionamento operante&quot;." ID="ID_445732855" CREATED="1460188785628" MODIFIED="1460188982806"/>
+<node TEXT="Condizionamento operante: Lo stimolo dell&apos;ambiente &#xe8; la risposta ad un comportamento e questa produce un rinforzo (punizione/gratificazione). La punizione tende ad inibire un comportamento mentre la gratificazione lo rende pi&#xf9; probanile.&#xa;Il comportamento operante viene selezionato dalle sue conseguenze cnosentendo l&apos;adattamento all&apos;ambiente. Cos&#xec; come la selezione naturale (Darwin) seleziona gli organismi in base alla capacit&#xe0; di adattarsi all&apos;ambiente attraverso la riproduzione, allo stesso modo i comportamenti degli organismi sono selezionati dalla loro capacit&#xe0; di adattarsi all&apos;ambiente attraverso le loro conseguenze." ID="ID_435033588" CREATED="1460189808847" MODIFIED="1460190149590"/>
+</node>
+<node TEXT="Bandura" ID="ID_761933222" CREATED="1460190166001" MODIFIED="1460190168047">
+<node TEXT="1925 - Canada - Psicologo" ID="ID_515625935" CREATED="1460190442678" MODIFIED="1460190456979"/>
+</node>
 </node>
 <node TEXT="Cognitivismo" ID="ID_1346016008" CREATED="1460183004683" MODIFIED="1460183008767"/>
 <node TEXT="Psicologia della &quot;Gestalt&quot;" ID="ID_784269495" CREATED="1460182985548" MODIFIED="1460183000622"/>
@@ -104,7 +136,34 @@
 </node>
 <node TEXT="Competenze didattiche del docente" ID="ID_945474912" CREATED="1460182580052" MODIFIED="1460182589217"/>
 <node TEXT="Progettazione del curricolo" ID="ID_1021202671" CREATED="1460182592644" MODIFIED="1460182602798"/>
-<node TEXT="Tecnologie per la didattica" ID="ID_393171228" CREATED="1460182605192" MODIFIED="1460182611260"/>
+<node TEXT="Tecnologie per la didattica" ID="ID_393171228" CREATED="1460182605192" MODIFIED="1460182611260">
+<node TEXT="Moodle" ID="ID_625046877" CREATED="1460190829138" MODIFIED="1460190831872">
+<node TEXT="Pedagogia sociale costruzionista" ID="ID_1390088699" CREATED="1460190839052" MODIFIED="1460190935029">
+<node TEXT="Costruttivismo" ID="ID_1277628995" CREATED="1460190858180" MODIFIED="1460190886114">
+<node TEXT="Le persone costruiscono attivamente nuova conoscenza mediante l&apos;interazione con l&apos;ambiente. Tutto quello che viene visto, letto, sentito e toccato &#xe8; confrontato con la conoscenza precedente e viene assorbito nel mondo della mente formando nuova conoscenza ma l&apos;apprendimento richiede un ruolo attivo: l&apos;interpretazione." ID="ID_288433183" CREATED="1460191001445" MODIFIED="1460191221578"/>
+</node>
+<node TEXT="Costruzionismo" ID="ID_1441107220" CREATED="1460190886592" MODIFIED="1460190900003">
+<node TEXT="L&apos;apprendimento &#xe8; particolarmente efficacie quando si costruisce qualcosa a partire dalle esperienze altrui. Queste vanno ristrutturate e ricostruite con i propri concetti. Prendere appunti serve a questo." ID="ID_1797177882" CREATED="1460191252530" MODIFIED="1460191390088"/>
+</node>
+<node TEXT="Costruzionismo sociale" ID="ID_243466972" CREATED="1460190909536" MODIFIED="1460190915125">
+<node TEXT="La conoscenza pu&#xf2; emergere da gruppi di persone che costruiscono conoscenza collaborativamente gli uni con gli altri, creando una cultura con significati condivisi. Questo stimola l&apos;apprendimento su pi&#xf9; livelli" ID="ID_1038532052" CREATED="1460191424001" MODIFIED="1460191550530"/>
+<node TEXT="Chiunque &#xe8; un potenziale insegnante e un potenziale discente" ID="ID_650033698" CREATED="1460192277785" MODIFIED="1460192301589"/>
+<node TEXT="Chiunque apprende meglio un concetto quando lo spiega agli altri (imparare facendo)" ID="ID_878930458" CREATED="1460192322193" MODIFIED="1460192517414"/>
+<node TEXT="Chiunque apprende dall&apos;osservazione del comportamento dei propri pari (apprendimento per osmosi)" ID="ID_966894619" CREATED="1460192420584" MODIFIED="1460192465589"/>
+<node TEXT="Conoscendo il contesto degli altri si pu&#xf2; agire in modo da indurre trasformazioni pi&#xf9; incisive" ID="ID_506302565" CREATED="1460192610941" MODIFIED="1460192643544"/>
+<node TEXT="" ID="ID_1936035723" CREATED="1460192685030" MODIFIED="1460192685030"/>
+</node>
+<node TEXT="Comportamento" ID="ID_1649271124" CREATED="1460191605970" MODIFIED="1460191775882">
+<node TEXT="Separato: ognuno prova ad essere oggettivo e ad attenersi ai fatti, difendendo le proprie idee usando la logica per validare le proprie idee e confutare le idee degli altri" ID="ID_1428157597" CREATED="1460191614244" MODIFIED="1460191780783"/>
+<node TEXT="Collegato: ognuno accetta la soggettivit&#xe0; e prova a comprendere i punti di vista degli altri" ID="ID_121810209" CREATED="1460191746202" MODIFIED="1460191844899"/>
+<node TEXT="Costruito: ognuno &#xe8; sensibile ad entrambi gli approcci e scegli quale comportamento adottare in base alla situazione corrente. Per essere efficacie deve condurre ad una riflessione profonda e ad un riesame delle conoscenze pregresse" ID="ID_1379791265" CREATED="1460191845593" MODIFIED="1460191991194"/>
+</node>
+<node TEXT="Pedagogia" ID="ID_1900812311" CREATED="1460192145570" MODIFIED="1460192152942">
+<node TEXT="La pedagogia si riferisce all&apos;arte (o scienza) di insegnare, alle strategie d&apos;insegnamento e ai suoi stili" ID="ID_181058700" CREATED="1460192153716" MODIFIED="1460192214587"/>
+</node>
+</node>
+</node>
+</node>
 <node TEXT="Competenze sociali del docente" ID="ID_533672421" CREATED="1460182617341" MODIFIED="1460182624227"/>
 <node TEXT="Stili di apprendimento e d&apos;insegnamento" ID="ID_1027383657" CREATED="1460182624467" MODIFIED="1460182644590"/>
 <node TEXT="Bisogni educativi speciali" ID="ID_71782056" CREATED="1460182645055" MODIFIED="1460182655746"/>
