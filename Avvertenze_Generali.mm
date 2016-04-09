96e7880fe5b493fbbba9aaafd1821e46d0046ea1
Gionata Massi (in qualità di docente d'Informatica) <gionata.massi@savoiabenincasa.it>
2016-04-09 08:53:38 +0200

    Initial commit

---
+++ Avvertenze_Generali.mm
@@ -0,0 +1,133 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Avvertenze Generali" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1460184698749"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4"/>
+<node TEXT="Competenze pedagogico-didattiche" POSITION="right" ID="ID_155819811" CREATED="1460182485131" MODIFIED="1460182504371">
+<edge COLOR="#ff0000"/>
+<node TEXT="Teorie dell&apos;apprendimento e psicologia dell&apos;educazione" ID="ID_1422292693" CREATED="1460182527908" MODIFIED="1460182552199">
+<node TEXT="Comportamentismo" ID="ID_1015287476" CREATED="1460182964837" MODIFIED="1460182970282">
+<node TEXT="Introduzione" ID="ID_1283369102" CREATED="1460184467648" MODIFIED="1460184475461">
+<node ID="ID_816649685" CREATED="1460184520253" MODIFIED="1460184677422"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Il comportamentismo studia il meccanismo Stimolo S (esterno) -&gt; Risposta R (comportamento). Quando la risposta R ad un dato stimolo S diviene stabile si ha l'<b>apprendimento</b>.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Pavlov" ID="ID_1187425509" CREATED="1460184476208" MODIFIED="1460184480108"/>
+<node TEXT="Thorndike" ID="ID_544706949" CREATED="1460184483383" MODIFIED="1460184512715"/>
+<node TEXT="Skinner" ID="ID_1493424747" CREATED="1460184513574" MODIFIED="1460184517306"/>
+</node>
+<node TEXT="Cognitivismo" ID="ID_1346016008" CREATED="1460183004683" MODIFIED="1460183008767"/>
+<node TEXT="Psicologia della &quot;Gestalt&quot;" ID="ID_784269495" CREATED="1460182985548" MODIFIED="1460183000622"/>
+<node TEXT="Human Information Processing" ID="ID_1365890437" CREATED="1460183016257" MODIFIED="1460183028269"/>
+<node TEXT="Costruttivismo" ID="ID_1268805831" CREATED="1460183039830" MODIFIED="1460183043594"/>
+</node>
+<node TEXT="Psicologia dello sviluppo" ID="ID_851841057" CREATED="1460182552488" MODIFIED="1460182563310">
+<node TEXT="Teorie di Piaget" ID="ID_1016905559" CREATED="1460183060075" MODIFIED="1460183067286"/>
+<node TEXT="Teorie di Vygotskij" ID="ID_280345649" CREATED="1460183072857" MODIFIED="1460183090619"/>
+<node TEXT="Teorie di Bruner" ID="ID_287570668" CREATED="1460183091206" MODIFIED="1460183104936"/>
+<node TEXT="Teorie di Freud" ID="ID_1850667533" CREATED="1460183110955" MODIFIED="1460183120741"/>
+<node TEXT="Teorie di Erikson" ID="ID_1429163820" CREATED="1460183121273" MODIFIED="1460183136435"/>
+<node TEXT="Teorie di Bowlby" ID="ID_341283630" CREATED="1460183669799" MODIFIED="1460183905005"/>
+<node TEXT="Teorie di Ainsworth" ID="ID_1043561937" CREATED="1460183678565" MODIFIED="1460183969339"/>
+<node TEXT="Teorie di Kohlberg" ID="ID_1022078825" CREATED="1460183697083" MODIFIED="1460184218337"/>
+<node TEXT="Teorie di Selman" ID="ID_488557596" CREATED="1460183699384" MODIFIED="1460184225161"/>
+</node>
+<node TEXT="Competenze psico-pedagogiche" ID="ID_850034429" CREATED="1460182564343" MODIFIED="1460182576788">
+<node TEXT="Dewey" ID="ID_927865662" CREATED="1460184258395" MODIFIED="1460184268263"/>
+<node TEXT="Skinner" ID="ID_1483260479" CREATED="1460184273001" MODIFIED="1460184279077"/>
+<node TEXT="Bloom" ID="ID_1683379924" CREATED="1460184281096" MODIFIED="1460184284116"/>
+<node TEXT="Piaget" ID="ID_282344770" CREATED="1460184291422" MODIFIED="1460184294323"/>
+<node TEXT="Vygotskij" ID="ID_689152552" CREATED="1460184294590" MODIFIED="1460184333525"/>
+<node TEXT="Bruner" ID="ID_1233762519" CREATED="1460184334104" MODIFIED="1460184345548"/>
+<node TEXT="von Glaserfeld" ID="ID_461514862" CREATED="1460184354854" MODIFIED="1460184374762"/>
+<node TEXT="von Foerster" ID="ID_1491367844" CREATED="1460184375645" MODIFIED="1460184384866"/>
+<node TEXT="Morin" ID="ID_429874117" CREATED="1460184411859" MODIFIED="1460184414728"/>
+</node>
+<node TEXT="Competenze didattiche del docente" ID="ID_945474912" CREATED="1460182580052" MODIFIED="1460182589217"/>
+<node TEXT="Progettazione del curricolo" ID="ID_1021202671" CREATED="1460182592644" MODIFIED="1460182602798"/>
+<node TEXT="Tecnologie per la didattica" ID="ID_393171228" CREATED="1460182605192" MODIFIED="1460182611260"/>
+<node TEXT="Competenze sociali del docente" ID="ID_533672421" CREATED="1460182617341" MODIFIED="1460182624227"/>
+<node TEXT="Stili di apprendimento e d&apos;insegnamento" ID="ID_1027383657" CREATED="1460182624467" MODIFIED="1460182644590"/>
+<node TEXT="Bisogni educativi speciali" ID="ID_71782056" CREATED="1460182645055" MODIFIED="1460182655746"/>
+<node TEXT="Continuit&#xe0;, orientamento e valutazione" ID="ID_1562880238" CREATED="1460182656163" MODIFIED="1460182664312"/>
+</node>
+<node TEXT="Legislazione scolastica" POSITION="right" ID="ID_226234241" CREATED="1460182505790" MODIFIED="1460182513508">
+<edge COLOR="#0000ff"/>
+<node TEXT="Storia delle istituzioni scolastiche italiane" ID="ID_144615287" CREATED="1460182666458" MODIFIED="1460182685122"/>
+<node TEXT="Infanzia e primo ciclo" ID="ID_76584016" CREATED="1460182697698" MODIFIED="1460182703827"/>
+<node TEXT="Caratteri comuni del secondo ciclo" ID="ID_1303380041" CREATED="1460182708678" MODIFIED="1460182736884"/>
+<node TEXT="Ordinamenti del secondo ciclo" ID="ID_162241092" CREATED="1460182737336" MODIFIED="1460182750457"/>
+<node TEXT="Legislazione europea" ID="ID_1468137824" CREATED="1460182751716" MODIFIED="1460182764805"/>
+<node TEXT="Autonomia e dirigenza" ID="ID_211920850" CREATED="1460182765248" MODIFIED="1460182775916"/>
+<node TEXT="Organi collegiali d&apos;Istituto" ID="ID_1668961287" CREATED="1460182802103" MODIFIED="1460182810586"/>
+<node TEXT="Stato giuridico e profilo contrattuale del docente" ID="ID_1447885608" CREATED="1460182815811" MODIFIED="1460182827878"/>
+<node TEXT="Studenti con BES" ID="ID_1144959256" CREATED="1460182833062" MODIFIED="1460182839930"/>
+</node>
+<node TEXT="Pubblica Amministrazione" POSITION="right" ID="ID_306187437" CREATED="1460182845252" MODIFIED="1460182855422">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Ordinamento dello stato e MIUR" ID="ID_2603679" CREATED="1460182856929" MODIFIED="1460182872906"/>
+<node TEXT="Autonomie territoriali" ID="ID_1615137990" CREATED="1460182873316" MODIFIED="1460182885287"/>
+<node TEXT="Regole della PA" ID="ID_1059735667" CREATED="1460182885777" MODIFIED="1460182899454"/>
+<node TEXT="Rapporto di lavoro nella PA" ID="ID_365454733" CREATED="1460182899920" MODIFIED="1460182905558"/>
+</node>
+</node>
+</map>
